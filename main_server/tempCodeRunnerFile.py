bda65cd03c1961e63ce8b29e822ee64504758c5f
kiranimmadi2 <kiranimmadi2@gmail.com>
2024-02-16 22:54:21 +0530

    FIRST COMMIT
    THIS IS MY FIRST COMMIT

---
+++ main_server/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+# Include your Infura API key in the headers
+    headers = {
+        "Authorization": "Bearer b2a4f441d8c14afb97a4bdca0281bbd7"  # Replace with your actual Infura API key  (Bearer )
+    }
